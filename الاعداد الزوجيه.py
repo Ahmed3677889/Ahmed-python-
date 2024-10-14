714506ad4660fbe3f6b57047dc1bcb37d46352a2
Ahmed3677889 <Ahmedwork1231@gmail.com>
2024-10-15 00:00:25 +0300

    الاعداد الفرديةه.py

--- الاعداد الزوجيه.py
+++ الاعداد الزوجيه.py
@@ -1,7 +1,7 @@
 def print_even_numbers():
     print("الأعداد الزوجية من ١ إلى ١١ هي:")
     for num in range(1, 12):
-        if num % 2 == 0:
+        if num % 2 != 0:
             print(num)
 
 # Call the function
