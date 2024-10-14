ee677774f6fa0bb777b2cab9c94054837d23a5d9
Ahmed3677889 <Ahmedwork1231@gmail.com>
2024-10-14 23:58:33 +0300

    Add files via upload

---
+++ الاعداد الزوجيه.py
@@ -0,0 +1,8 @@
+def print_even_numbers():
+    print("الأعداد الزوجية من ١ إلى ١١ هي:")
+    for num in range(1, 12):
+        if num % 2 == 0:
+            print(num)
+
+# Call the function
+print_even_numbers()
